fb43680b0439954c8742d81aa91740a8418951ca
Jon Austen <djangofan@gmail.com>
2023-08-02 17:25:21 -0700

    Create download_jars.sh
    Test

---
+++ download_jars.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget https://repo.maven.apache.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.10.2/spring-cloud-dataflow-server-2.10.2.jar
+
+wget https://repo.maven.apache.org/maven2/org/springframework/cloud/spring-cloud-dataflow-shell/2.10.2/spring-cloud-dataflow-shell-2.10.2.jar
+
+wget https://repo.maven.apache.org/maven2/org/springframework/cloud/spring-cloud-skipper-server/2.9.2/spring-cloud-skipper-server-2.9.2.jar
+
