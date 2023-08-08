40ec06ac531d555e383018ad1962d00f59081cec
Jon Austen <djangofan@gmail.com>
2023-08-08 12:38:31 -0700

    Update 2.start_server.sh

--- server/2.start_server.sh
+++ server/2.start_server.sh
@@ -2,7 +2,7 @@
 
 # in IntelliJ-IDEA, just right click and run this file
 
-mvn install:install-file -DgroupId=io.spring -DartifactId=timestamp-task -Dversion=2.02 -Dpackaging=jar -Dfile=./timestamp-task-2.1.1.RELEASE.jar
+# mvn install:install-file -DgroupId=io.spring -DartifactId=timestamp-task -Dversion=2.02 -Dpackaging=jar -Dfile=./timestamp-task-2.1.1.RELEASE.jar
 
 java -jar spring-cloud-dataflow-server-2.10.2.jar
 
