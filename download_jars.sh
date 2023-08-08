faa4e4cb8d5c229f1f74336e41494e217654213a
Jon Austen <djangofan@gmail.com>
2023-08-08 13:30:22 -0700

    Rearrange files

--- download_jars.sh
+++ download_jars.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 # curl is included in Git For Windows, if you run this script on windows
 
-cd server
 curl -LO https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.10.3/spring-cloud-dataflow-server-2.10.3.jar
-cd ..
-cd shell
-curl -LO https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-shell/2.10.3/spring-cloud-dataflow-shell-2.10.3.jar
-cd ..
-cd skipper
-curl -LO https://repo.maven.apache.org/maven2/org/springframework/cloud/spring-cloud-skipper-server/2.9.3/spring-cloud-skipper-server-2.9.3.jar
 
-cd ..
+curl -LO https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-shell/2.10.3/spring-cloud-dataflow-shell-2.10.3.jar
+
+curl -LO https://repo.maven.apache.org/maven2/org/springframework/cloud/spring-cloud-skipper-server/2.9.3/spring-cloud-skipper-server-2.9.3.jar
 
 echo "End"
 read -p "Press enter to continue"
