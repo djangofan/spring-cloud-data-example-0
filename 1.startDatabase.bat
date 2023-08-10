241101651339f9b241b67f1cc9242337586d02b6
Jon Austen <djangofan@gmail.com>
2023-08-09 17:53:26 -0700

    minor edit

--- 1.startDatabase.bat
+++ 1.startDatabase.bat
@@ -1,4 +1,5 @@
-
+ECHO off
 docker run -d --hostname rabbitmq --name rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3.12.2-management
 
-docker run --name test-mariadb -d -p 3306:3306 -e MARIADB_ROOT_PASSWORD=pass -e MARIADB_USER=user -e MARIADB_PASSWORD=user -e MARIADB_DATABASE=dataflow mariadb:10.4.22
+docker run --name test-mariadb -d -p 3306:3306 -e MARIADB_ROOT_PASSWORD=pass -e MARIADB_USER=dfuser -e MARIADB_PASSWORD=password -e MARIADB_DATABASE=dataflow mariadb:10.4.22
+
